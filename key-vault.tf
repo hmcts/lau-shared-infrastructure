8fe5e084da6480d8a87cc295f40fc262700c3e0c
Dave Jones <dave.jones@hmcts.net>
2021-08-23 12:57:50 +0100

    LAU-56: Resolve infrastrucure build.

--- key-vault.tf
+++ key-vault.tf
@@ -8,7 +8,8 @@ module "lau-vault" {
   resource_group_name        = azurerm_resource_group.rg.name
 
   # dcd_group_lau_v2 group object ID
-  product_group_object_id    = "20f0f9b1-1953-43c0-9e68-91d6674a6acc"
+  #product_group_object_id    = "20f0f9b1-1953-43c0-9e68-91d6674a6acc"
+  product_group_object_name  = "DTS LAU"
   common_tags                = var.common_tags
   create_managed_identity    = true
 }
