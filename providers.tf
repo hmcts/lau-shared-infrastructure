a2ba911e81274cd9b319ed5240a71fbdb4c500be
Dave Jones <dave.jones@hmcts.net>
2021-08-23 11:07:35 +0100

    LAU-56: Resolve infrastrucure build.

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
   alias           = "aks-infra"
   subscription_id = var.aks_infra_subscription_id
   features {}
-  skip_provider_registration = true
+  skip_provider_registration = false
 }
 
 provider "azurerm" {
