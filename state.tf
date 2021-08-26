dc814be1ec915ab65bd65cc04785672557cab50e
bradley-hmcts <61189290+bradley-hmcts@users.noreply.github.com>
2021-08-26 17:36:04 +0100

    Fix azuread

--- state.tf
+++ state.tf
@@ -1,10 +1,20 @@
 terraform {
+  required_providers {
+    
+  }
+}
+
+terraform {
   backend "azurerm" {}
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.25"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "1.6.0"
     }
   }
 }
