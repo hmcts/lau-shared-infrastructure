d0e83154021b7a325ae254385c9c3fb01a98d1f2
Martin <martin.spasov@hmcts.net>
2022-10-19 15:31:44 +0100

    Cahnge search query

--- alerts.tf
+++ alerts.tf
@@ -8,7 +8,6 @@ module "case-disposer-action-group" {
   env                    = var.env
   resourcegroup_name     = local.alert_resource_group_name
   action_group_name      = "${var.application_name}-${var.env}-support"
-  short_name             = "cd${var.env}Sup"
   email_receiver_name    = "Case Disposer Deletion Failure Alert"
   email_receiver_address = data.azurerm_key_vault_secret.caseDisposerAlertEmail.value
 }
@@ -19,7 +18,7 @@ module "case-disposer-deletion-failure-alert" {
   app_insights_name          = "ccd-${var.env}"
   alert_name                 = "${var.application_name}-${var.env}-deletion-failure"
   alert_desc                 = "Alert when case disposer fail to delete case data"
-  app_insights_query         = "traces | where message contains 'Case Disposer Deletion Summary' and message !contains 'Failed cases : 0'"
+  app_insights_query         = "traces | where message contains 'Case Disposer Deletion Summary'"
   custom_email_subject       = "Alert: Case disposer deletion failure"
   #run every 15 hrs for early alert
   frequency_in_minutes       = 15
