c1e34549e140e30b4c140bf27604b1f6913c1b57
Martin <martin.spasov@hmcts.net>
2022-10-20 14:05:20 +0100

    Change alarm name

--- alerts.tf
+++ alerts.tf
@@ -7,7 +7,7 @@ module "case-disposer-action-group" {
   location               = "global"
   env                    = var.env
   resourcegroup_name     = local.alert_resource_group_name
-  action_group_name      = "${var.application_name}-${var.env}-support"
+  action_group_name      = "${var.application_name}-${var.env}"
   short_name             = "cd${var.env}Sup"
   email_receiver_name    = "Case Disposer Deletion Failure Alert"
   email_receiver_address = data.azurerm_key_vault_secret.caseDisposerAlertEmail.value
@@ -17,7 +17,7 @@ module "case-disposer-deletion-failure-alert" {
   source                     = "git@github.com:hmcts/cnp-module-metric-alert"
   location                   = var.appinsights_location
   app_insights_name          = "ccd-${var.env}"
-  alert_name                 = "${var.application_name}-${var.env}-deletion-failure"
+  alert_name                 = "${var.application_name}-${var.env}-deletion-failures"
   alert_desc                 = "Alert when case disposer fail to delete case data"
   app_insights_query         = "traces | where message contains 'Case Disposer Deletion Summary'"
   custom_email_subject       = "Alert: Case disposer deletion failure"
