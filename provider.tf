271c2ab211a0d4c64fdbb0e95d267e7d991edb60
priyankaVerma21 <priyanka.verma@HMCTS.NET>
2025-09-15 11:23:12 +0100

    LAU-1250 Remove skip_provider_registration (#154)
    * Replace with resource_provider
    
    * Updating Terraform Formatting
    
    * Remove and let terraform choose default
    
    * Updating Terraform Formatting
    
    ---------
    
    Co-authored-by: 54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 provider "azurerm" {
   features {}
-  alias                      = "aks"
-  subscription_id            = var.aks_subscription_id
-  skip_provider_registration = false
+  alias           = "aks"
+  subscription_id = var.aks_subscription_id
 }
 
 provider "azurerm" {
