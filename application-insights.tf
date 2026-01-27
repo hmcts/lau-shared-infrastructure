ac3963a139bfc6a90fb82719d71659369725f9d8
Priyanka Verma <priyanka.verma@hmcts.net>
2026-01-27 10:45:47 +0000

    LAU-1348 moving appInsights to default location UK south

--- application-insights.tf
+++ application-insights.tf
@@ -3,7 +3,7 @@ module "application_insights" {
 
   env                 = var.env
   product             = var.product
-  location            = var.appinsights_location
+  location            = var.location
   application_type    = var.application_type
   resource_group_name = azurerm_resource_group.rg.name
 
