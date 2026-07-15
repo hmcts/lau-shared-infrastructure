811a154ffc55bb28a0130a1bb94a5d6d77f92ebd
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-07-15 17:08:21 +0100

    Address review comments

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "lau-vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
   name                    = "lau-${var.env}"
   product                 = var.product
   env                     = var.env
@@ -25,25 +25,6 @@ module "lau-vault" {
   product_group_name      = "DTS LAU"
   common_tags             = var.common_tags
   create_managed_identity = true
-}
-
-data "azurerm_client_config" "current" {}
-
-data "azurerm_user_assigned_identity" "jenkins_preview" {
-  count               = var.env == "aat" ? 1 : 0
-  name                = "jenkins-preview-mi"
-  resource_group_name = "managed-identities-preview-rg"
-}
-
-resource "azurerm_key_vault_access_policy" "jenkins_preview" {
-  count        = var.env == "aat" ? 1 : 0
-  key_vault_id = module.lau-vault.key_vault_id
-  object_id    = data.azurerm_user_assigned_identity.jenkins_preview[0].principal_id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-
-  key_permissions         = ["Get", "List"]
-  certificate_permissions = ["Get", "List"]
-  secret_permissions      = ["Get", "List"]
 }
 
 output "vaultName" {
